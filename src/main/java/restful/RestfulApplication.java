5be6da218cd043d0230cfa269e85fdeec616358f
ws88 <657892503@.com>
2019-09-28 00:30:38 +0800

    为啥加布上去呢

--- src/main/java/restful/RestfulApplication.java
+++ src/main/java/restful/RestfulApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //加了个注解
 //你好你好
+//日了狗了
 @SpringBootApplication
 public class RestfulApplication {
 
