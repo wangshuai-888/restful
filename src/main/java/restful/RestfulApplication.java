55a93ae998b11b65d35dd52137ce4d36a21a4da2
wangshuai-888 <55477898+wangshuai-888@users.noreply.github.com>
2019-09-28 00:32:19 +0800

    Update RestfulApplication.java

--- src/main/java/restful/RestfulApplication.java
+++ src/main/java/restful/RestfulApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//尼玛个批
 public class RestfulApplication {
 
     public static void main(String[] args) {
