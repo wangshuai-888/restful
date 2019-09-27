6967a690fea9b7148873c13de143dd3acde7ae0d
ws88 <657892503@.com>
2019-09-28 00:24:02 +0800

    頂頂頂

--- src/main/java/restful/RestfulApplication.java
+++ src/main/java/restful/RestfulApplication.java
@@ -2,7 +2,7 @@ package restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//加了个注解
 @SpringBootApplication
 public class RestfulApplication {
 
